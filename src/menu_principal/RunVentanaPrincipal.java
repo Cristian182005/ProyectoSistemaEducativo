f60e569b85219f266d9d484d285a6a0390213f95
JanusCross15 <csrjr15@gmail.com>
2025-06-18 00:45:36 -0500

    avance de ventanas

---
+++ src/menu_principal/RunVentanaPrincipal.java
@@ -0,0 +1,11 @@
+package menu_principal;
+
+public class RunVentanaPrincipal {
+
+    public static void main(String[] args) {
+
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+        ventanaPrincipal.setVisible(true);
+    }
+    
+}
